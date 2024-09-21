760b67bf2102e360a78126af35a63b5320b9e478
para-commando <anirudh.nayak.172k@gmail.com>
2024-09-21 13:52:01 +0530

    redundancy cleanup

--- kafka-consumer-service/src/app.module.ts
+++ kafka-consumer-service/src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA_CLIENT',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['localhost:9092'],
-            clientId: 'my-consumer-client',
-          },
-          consumer: {
-            groupId: 'my-kafka-consumer-group',
-            sessionTimeout: 90000, // large enough to fit any message being processed
-            heartbeatInterval: 30000,
-            allowAutoTopicCreation: true,
-            // Disable auto-commit to manage acknowledgements manually
-          },
-        },
-      },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
